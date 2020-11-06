a6bff4eca3b064b566f936dd94f4e7fd706109da
Kaushik Velusamy <velusamy1@llnl.gov>
2020-11-05 16:32:26 -0800

    Added metall fallback adaptor: Temporary non metall types fallback to the default heap allocator (malloc)

--- src/demo/triangle_count_demo.cpp
+++ src/demo/triangle_count_demo.cpp
@@ -37,7 +37,7 @@
 
 
 #include <metall/metall.hpp>
-
+#include <metall_utility/fallback_allocator_adaptor.hpp>
 
 
 //****************************************************************************
@@ -142,7 +142,8 @@ int main(int argc, char **argv)
     }
 
     using T = int32_t;
-    using Metall_MatType = grb::Matrix<T, metall::manager::allocator_type<char>>;
+    using allocator_t = metall::utility::fallback_allocator_adaptor<metall::manager::allocator_type<char>>;
+    using Metall_MatType = grb::Matrix<T, allocator_t>;
     //using MatType = grb::Matrix<T, grb::DirectedMatrixTag>;
 
     grb::IndexType NUM_NODES(max_id + 1);
